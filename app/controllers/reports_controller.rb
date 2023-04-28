c48a22bf22c9e939d34f015a8655932fb99790a8
eugeny <ltv1304@yande.ru>
2023-04-28 17:00:45 +0300

    add: views and controllers

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,4 +1,6 @@
 class ReportsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
+  
   def index
     @reports = Report.all
   end
@@ -16,8 +18,25 @@ class ReportsController < ApplicationController
   end
 
   def update
+    @report.editions_count += 1
+    if @report.update post_params
+      redirect_to action: "index"
+    else
+      # flash.now[:danger] = 'Статья не обновлена'
+      render :edit
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def set_post
+    @report = Report.find(params[:id])
+  end
+
+  def post_params
+    params.require(:report).permit(:title, :content)
   end
 end
